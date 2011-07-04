de4f6db08a92467a904416d7565c0b961c3b248a
Vasil Vangelovski <vvangelovski@gmail.com>
2011-07-04 13:36:32 +0200

    Added cookbook for redis

---
+++ cookbooks/redis/recipes/default.rb
@@ -0,0 +1,71 @@
+package "build-essential" do
+  action :install
+end
+
+user node[:redis][:user] do
+  action :create
+  system true
+  shell "/bin/false"
+end
+
+directory node[:redis][:dir] do
+  owner "root"
+  mode "0755"
+  action :create
+end
+
+directory node[:redis][:data_dir] do
+  owner "redis"
+  mode "0755"
+  action :create
+end
+
+directory node[:redis][:log_dir] do
+  mode 0755
+  owner node[:redis][:user]
+  action :create
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/redis.tar.gz" do
+  source "https://github.com/antirez/redis/tarball/v2.0.4-stable"
+  action :create_if_missing
+end
+
+bash "compile_redis_source" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    tar zxf redis.tar.gz
+    cd antirez-redis-55479a7
+    make && make install
+  EOH
+  creates "/usr/local/bin/redis-server"
+end
+
+service "redis" do
+  provider Chef::Provider::Service::Upstart
+  subscribes :restart, resources(:bash => "compile_redis_source")
+  supports :restart => true, :start => true, :stop => true
+end
+
+template "redis.conf" do
+  path "#{node[:redis][:dir]}/redis.conf"
+  source "redis.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, resources(:service => "redis")
+end
+
+template "redis.upstart.conf" do
+  path "/etc/init/redis.conf"
+  source "redis.upstart.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, resources(:service => "redis")
+end
+
+service "redis" do
+  action [:enable, :start]
+end
+
