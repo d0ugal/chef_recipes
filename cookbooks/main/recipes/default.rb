7748be740e3763feefca070f34c2b64e235fbb17
Dougal Matthews <dougal85@gmail.com>
2011-09-12 17:39:10 +0100

    Done some tidying up to remove stuff that isn't needed and also add a few comments to the code.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,14 +1,22 @@
+=begin
+  This recipe is a utility recipe, installing a number of additions that are 
+  useful and required 90% of the time.
+=end
+
 execute "update-apt" do
     command "sudo apt-get update"
 end
 
-%w{ack-grep aptitude vim git-core subversion mercurial libxml2-dev}.each do |pkg|
+# Install Ack, Git, Hg, SVN, xml libs and jpeg libs.
+%w{ack-grep aptitude vim git-core subversion mercurial libxml2-dev 
+  libjpeg62-dev zlib1g-dev}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-# Allow the user to specify some other system packages to install
+# On a project basis, allow a set of packages to be defined that will be 
+# installed at almost the start of the setup process.
 if node.has_key?("system_packages")
   node[:system_packages].each do |pkg|
     package pkg do
@@ -17,6 +25,8 @@ if node.has_key?("system_packages")
   end
 end
 
+# Add a bashrc_extra file that is loaded in the bashrc and used to setup a few
+# aliases and other useful bash related features.
 cookbook_file "/home/vagrant/.bashrc_extra" do
   source "bashrc_extra"
   mode 0640
